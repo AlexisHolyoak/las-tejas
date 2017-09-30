2824da008a925e9002607251f51196ecf9f14564
alexisholyoak <peraltaholyoak@gmail.com>
2017-09-30 17:02:45 -0500

    test 8

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,7 @@ Route::get('/', function () {
 });
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/platillos','PlatillosController@index')->name('platillos');
+Route::get('/platillos',[
+  'as'  => 'platillos',
+  'uses'=> 'PlatillosController@index'
+]);
