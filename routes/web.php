2ad73dc8a0c0ae5331ee43ce6fd5b7e0754b382c
alexisholyoak <peraltaholyoak@gmail.com>
2017-09-30 13:59:09 -0500

    test 6

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 Auth::routes();
-Route::get('/login', 'HomeController@index')->name('login');
-Route::get('/register', function () {
-    return view ('auth.register');
-})->name('register');
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/login/home', [
+    'as'   => 'login.home',
+    'uses' => 'HomeController@index'
+]);
