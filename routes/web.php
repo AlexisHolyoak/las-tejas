11c6464c4382cedc7896ad853928122991ff9580
alexisholyoak <peraltaholyoak@gmail.com>
2017-09-30 22:30:58 -0500

    ....

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('platillos','CocinerosController@platillos')->name('platillos');
 Route::get('ordenes','CocinerosController@ordenes')->name('ordenes');
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
-Route::view('cocineros','PlatillosController@index')->name('cocineros')
+Route::view('cocineros','PlatillosController@index')->name('cocineros');
