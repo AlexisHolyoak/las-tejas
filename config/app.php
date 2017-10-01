61e6c52136bb1dfed0be028492b62228e3902ac5
alexisholyoak <peraltaholyoak@gmail.com>
2017-09-30 22:44:41 -0500

    url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://las-tejas-pr-6.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
