782f1084d53e4204ffd2c8fd4abd1473688fa478
alexisholyoak <peraltaholyoak@gmail.com>
2017-10-01 00:19:11 -0500

    routes loop

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('register/',function(){
+  return view('auth.register');
+})->name('register');
+Route::get('register',function(){
+  return redirect::to('register/')
+});
