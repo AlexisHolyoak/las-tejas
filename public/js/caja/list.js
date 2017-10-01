529f90ed610933d62e9c10880ee0408085a87b09
Alvaro RC <alvaro.resurreccion@2myr.com>
2017-10-01 11:29:06 -0500

    feat(caja): Creación de archivos y avance de maquetación del prototipo de la vista

---
+++ public/js/caja/list.js
@@ -0,0 +1,18 @@
+// $(document).ready(function(){
+//     $('.generate').on('click', onClickGenerate);
+// });
+//
+// function onClickGenerate() {
+//     var table_number = $(this).parents('.item').attr('data-table-id');
+//     $.ajax({
+//         type     : 'GET',
+//         datatype : 'json',
+//         url      : 'caja/generar/'+table_number,
+//         data     : '',
+//         success  : successByAkax,
+//     });
+// }
+//
+// function successByAkax(data) {
+//
+// }
