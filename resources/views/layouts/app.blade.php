9ae8d7781838023cced62794ae6461c3e5b9e616
alexisholyoak <peraltaholyoak@gmail.com>
2017-10-01 02:24:27 -0500

    header set to static-top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-fixed-top">
+        <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
 
