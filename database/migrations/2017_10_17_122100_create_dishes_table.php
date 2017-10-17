196340a1feb60d72999d377830e36baccce2dcc7
rdbc3415 <rdbc3415@gmail.com>
2017-10-17 00:34:12 -0500

    Agregue dishes

---
+++ database/migrations/2017_10_17_122100_create_dishes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Dishes', function (Blueprint $table) {
+          $table->increments('idDish');
+          $table->string('nameDish',30);
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Dishes');
+    }
+}
