564adb88c39583ac02e3f90b9ada090bcb44eeb6
alexisholyoak <peraltaholyoak@gmail.com>
2017-10-18 19:33:17 -0500

    Branches migration

---
+++ database/migrations/2017_10_18_192622_create_branches_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBranchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Branches', function (Blueprint $table) {
+            $table->increments('idBranch');
+            $table->string('address');
+            $table->string('observation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Branches');
+    }
+}
