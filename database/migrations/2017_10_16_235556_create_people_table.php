792d23f8b5e7f603a6226ef6e018f55851e5e73b
alexisholyoak <peraltaholyoak@gmail.com>
2017-10-18 19:10:41 -0500

    Migracion de la tabla People

---
+++ database/migrations/2017_10_16_235556_create_people_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('idPerson');
+            $table->string('personName',100);
+            $table->string('address');
+            $table->string('telephone',12);
+            $table->integer('idPosition')->unsigned();
+            $table->string('personType');
+            $table->integer('userType_idUserType')->unsigned();
+            $table->string('observations');
+            $table->string('state');
+            $table->string('registerDate');
+            $table->integer('branch_idBranch')->unsigned();
+            $table->integer('login_idLogin')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
