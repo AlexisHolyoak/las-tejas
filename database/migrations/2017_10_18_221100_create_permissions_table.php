2f865d4f2e6e3b26a9f6de9a27b8834887fda534
alexisholyoak <peraltaholyoak@gmail.com>
2017-10-18 23:21:42 -0500

    cambie el nombre de la tabla de grants a permissions

--- database/migrations/2017_10_18_221100_create_permissions_table.php
+++ database/migrations/2017_10_18_221100_create_permissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGrantsTable extends Migration
+class CreatePermissionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class CreateGrantsTable extends Migration
      */
     public function up()
     {
-        Schema::create('Grants', function (Blueprint $table) {
-            $table->increments('idGrant');
+        Schema::create('Permissions', function (Blueprint $table) {
+            $table->increments('idPermission');
             $table->string('name');
             $table->timestamps();
         });
@@ -27,6 +27,6 @@ class CreateGrantsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('Grants');
+        Schema::dropIfExists('Permissions');
     }
 }
