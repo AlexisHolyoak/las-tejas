55909983124630d35ee832fd1350dc05d1b242b2
alexisholyoak <peraltaholyoak@gmail.com>
2017-10-18 21:12:27 -0500

    Logins migration

---
+++ database/migrations/2017_10_18_210822_create_logins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoginsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Logins', function (Blueprint $table) {
+            $table->increments('idLogin');
+            $table->string('user');
+            $table->string('password');
+            $table->integer('roles_idRoles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Logins');
+    }
+}
