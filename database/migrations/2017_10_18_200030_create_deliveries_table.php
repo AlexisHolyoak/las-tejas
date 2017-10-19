ee474cecf3d744547fb1efaffa884dbe9ad620cc
alexisholyoak <peraltaholyoak@gmail.com>
2017-10-18 20:10:55 -0500

    Deliveries Migration

---
+++ database/migrations/2017_10_18_200030_create_deliveries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeliveriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Deliveries', function (Blueprint $table) {
+            $table->increments('idDelivery');
+            $table->date('estimatedTime');
+            $table->integer('idPerson');
+            $table->integer('idVoucher');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Deliveries');
+    }
+}
