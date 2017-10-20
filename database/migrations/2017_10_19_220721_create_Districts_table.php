08ed7bbead514b08076b428b9253202ecc54032b
VictorDza30 <vide_ali@hotmail.com>
2017-10-19 22:51:03 -0500

    Agregue Migraciones de Departaments, Provinces, Districts

---
+++ database/migrations/2017_10_19_220721_create_Districts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDistrictsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Districts', function (Blueprint $table) {
+            $table->increments('idDistrict');
+            $table->string('nameDistrict',100);
+            $table->integer('idProvince')->unsigned();
+            $table->foreign('idProvince')->references('idProvince')->on('Provinces')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Districts');
+    }
+}
