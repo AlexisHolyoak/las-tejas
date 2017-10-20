a080f32b0ca95bb764c67df2283c0bd558b99d0c
alexisholyoak <peraltaholyoak@gmail.com>
2017-10-20 00:40:38 -0500

    Le agregue la S a la clase preparations :C

--- database/migrations/2017_10_19_000015_create_preparations_table.php
+++ database/migrations/2017_10_19_000015_create_preparations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePreparationsTables extends Migration
+class CreatePreparationsTable extends Migration
 {
     /**
      * Run the migrations.
