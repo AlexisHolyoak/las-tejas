1aa4c3183f1c3df41a1185b8fd9e40c56fdd570b
VictorDza30 <vide_ali@hotmail.com>
2017-10-29 00:30:31 -0500

    Rutas Modulo 1

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,19 @@ Route::get('/platillos/',function(){
 Route::get('/mozo/',function(){
   return view('mozo.mesas');
 });
+Route::resource('branch', 'BranchController');
+Route::resource('auth', 'UserController');
+Route::get('auth/create', function(){
+	return view('auth.create');
+});
+Route::resource('role', 'RoleController');
+//AJAX UBIGEO
+Route::get('/ajax-departments/',function(){
+  return Response::json(lastejas\Department::all());
+});
+Route::get('/ajax-provinces/{id}',function($id){
+  return Response::json(lastejas\Province::where('idDepartment', $id)->get());
+});
+Route::get('/ajax-districts/{id}',function($id){
+  return Response::json(lastejas\District::where('idProvince', $id)->get());
+});
