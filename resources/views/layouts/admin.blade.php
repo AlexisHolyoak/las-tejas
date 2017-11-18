45aca1542b48a574aad793e96670500bf06eca22
apr01break <apr011097@gmail.com>
2017-11-18 12:21:21 -0500

    Portada agregada

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Agency - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="{{asset('vendor/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset('css/agency.min.css" rel="stylesheet')}}">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">APR</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ml-auto">
+
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="{{ route('login') }}">Iniciar Sesión</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead">
+      <div class="container">
+        <div class="intro-text">
+          <div class="intro-lead-in">Bienvenido a Las Tejas</div>
+          <div class="intro-heading text-uppercase">Gusto de conocerte</div>
+
+        </div>
+      </div>
+    </header>
+
+    <!-- Services -->
+
+    </footer>
+
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Contact form JavaScript -->
+    <script src="{{asset('js/jqBootstrapValidation.js')}}"></script>
+    <script src="{{asset('js/contact_me.js')}}"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="{{asset('js/agency.min.js')}}"></script>
+
+  </body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,54 +9,54 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layous.admin');
 });
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/ordenes/',function(){
-  return view('cocinero.ordenes');
+Route::get('/ordenes/', function () {
+    return view('cocinero.ordenes');
 });
-Route::prefix('platillos')->group(function(){
+Route::prefix('platillos')->group(function () {
     Route::get('/', 'Dishes\Dishes@index');
     Route::post('/agregar', 'Dishes\Dishes@store');
     Route::post('/edit/{id}', 'Dishes\Dishes@edit');
     Route::post('/update/{id}', 'Dishes\Dishes@update');
     Route::post('/delete/{id}', 'Dishes\Dishes@delete');
 });
-Route::get('/mozo/',function(){
-  return view('mozo.mesas');
+Route::get('/mozo/', function () {
+    return view('mozo.mesas');
 });
 Route::resource('branch', 'BranchController');
 Route::resource('auth', 'UserController');
 Route::post('/userrole/{id}', 'UserController@userrole');
-Route::get('auth/create', function(){
-	return view('auth.create');
+Route::get('auth/create', function () {
+    return view('auth.create');
 });
 Route::resource('role', 'RoleController');
 //AJAX UBIGEO
-Route::get('/ajax-departments/',function(){
-  return Response::json(lastejas\Department::all());
+Route::get('/ajax-departments/', function () {
+    return Response::json(lastejas\Department::all());
 });
-Route::get('/ajax-provinces/{id}',function($id){
-  return Response::json(lastejas\Province::where('idDepartment', $id)->get());
+Route::get('/ajax-provinces/{id}', function ($id) {
+    return Response::json(lastejas\Province::where('idDepartment', $id)->get());
 });
-Route::get('/ajax-districts/{id}',function($id){
-  return Response::json(lastejas\District::where('idProvince', $id)->get());
+Route::get('/ajax-districts/{id}', function ($id) {
+    return Response::json(lastejas\District::where('idProvince', $id)->get());
 });
-Route::resource('table','TableController');
-Route::get('/ajax-users/{id}',function($id){
-  $userroles=DB::table('Users as u')
-  ->join('UserRoles as ur','u.idUser','ur.idUser')
-  ->select('u.idUser as ID','u.nameUser as USER','u.firstSurNameUser as FIRST','u.secondSurNameUser as SECOND')
-  ->where('ur.idRole','=','2')
-  ->where('ur.idBranch',$id)
-  ->get();
-  return Response::json($userroles);
+Route::resource('table', 'TableController');
+Route::get('/ajax-users/{id}', function ($id) {
+    $userroles = DB::table('Users as u')
+        ->join('UserRoles as ur', 'u.idUser', 'ur.idUser')
+        ->select('u.idUser as ID', 'u.nameUser as USER', 'u.firstSurNameUser as FIRST', 'u.secondSurNameUser as SECOND')
+        ->where('ur.idRole', '=', '2')
+        ->where('ur.idBranch', $id)
+        ->get();
+    return Response::json($userroles);
 });
-Route::prefix('reports')->group(function(){
+Route::prefix('reports')->group(function () {
     Route::get('/', 'ReportController@index');
     Route::get('/user/pdf/{id}', 'ReportController@reportUsersPdf');
     Route::get('/user/excel', 'ReportController@reportUsersXls');
