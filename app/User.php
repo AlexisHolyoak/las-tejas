8fecff644657154818260404c804111b49181b0f
VictorDza30 <vide_ali@hotmail.com>
2017-11-26 13:59:40 -0500

    Middlewre Roles

--- app/User.php
+++ app/User.php
@@ -3,12 +3,14 @@ namespace lastejas;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use lastejas\Role;
+use DB;
 
 class User extends Authenticatable
 {
-    
+
     use Notifiable;
-    
+
     public $timestamp = true;
     protected $primaryKey = 'idUser';
     protected $table = 'Users';
@@ -19,4 +21,22 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function userroles(){
+        return $this->hasMany(UserRole::class,'idUser');
+    }
+    public function hasRole(array $roles){
+        $nameRoles = array();
+        foreach ($this->userroles as $r) {
+            if($r->statusUserRole==1){
+                $roleUser = Role::where('idRole',$r->idRole)->select('nameRole')->first()->nameRole;
+                foreach ($roles as $role) {
+                    if($roleUser==$role){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
