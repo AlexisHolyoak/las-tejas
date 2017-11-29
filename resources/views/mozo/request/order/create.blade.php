6f0062c0354b7e12903af3cee186e612ebaeeef9
Alexis Peralta <peraltaholyoak.aj@gmail.com>
2017-11-29 17:33:15 -0500

    Update create.blade.php

--- resources/views/mozo/request/order/create.blade.php
+++ resources/views/mozo/request/order/create.blade.php
@@ -86,7 +86,6 @@
         <div class="row border-bottom">
         <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
         <div class="navbar-header">
-            <a class="navbar-minimalize minimalize-styl-2 btn btn-default " href="#"><i class="fa fa-bars"></i></a>
         </div>
         <ul class="nav navbar-top-links navbar-right">
             <li>
